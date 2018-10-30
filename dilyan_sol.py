b6d6347a879b1c824ed8b97ce1d59e46062d1927
Botafogo1894 <pruchka@gmail.com>
2018-10-30 19:58:42 -0400

    far enough

---
+++ dilyan_sol.py
@@ -0,0 +1,13 @@
+from owner import Owner
+from car import Car
+
+jake = Owner("Jake", 25)
+jess = Owner("Jess", 29)
+alexa = Owner("Alexa", 33)
+pete = Owner("Pete", 30)
+
+camry = Car("Toyota", "Camry", 2014, jake)
+astra = Car("Opel", "Astra", 2000, jess)
+fiat = Car("Fiat", "Pop", 2012, alexa)
+mustang = Car("Ford", "Mustang", 2018, pete)
+cayene = Car("Porsche", "Cayene", 1998, alexa)
--- owner.py
+++ owner.py
@@ -1,10 +1,18 @@
-# import car class here
-
+from car import Car
 
 class Owner:
     def __init__(self, name, age):
         self._name = name
         self._age = age
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def year(self):
+        return self._year
+
     def find_my_cars(self):
-        pass
+        cars_w_name = list(filter(lambda item: item.owner.name == self.name, Car._all))
+        return list(map(lambda car: car.make + " " + car.model, cars_w_name))
